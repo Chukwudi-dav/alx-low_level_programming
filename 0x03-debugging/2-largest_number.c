cee0a44b3d9c38c010569b42038e6d3007263def
David <charleschukwudi2001@gmail.com>
2022-10-23 17:45:24 -0700

    My first commit

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+
+/ **
+
+ * largest_number - returns the largest number
+ *
+ *  * @a: first integer
+ *
+ *   * @b: second integer
+ *
+ *    * @c: third integer
+ *
+ *     * Return: largest number
+ *
+ *
+ */
+
+ int largest_number(int a, inb b, int c)
+
+{  
+
+	        int largest;
+
+		  
+
+		        if (a > b)
+
+				        {
+
+						                if (b > c)
+
+									                        largest = a;
+
+								                else if (a > c)
+
+											                        largest = a;
+
+										                else 
+
+													                        largest = c;
+
+
+
+
+
+												        }
+
+			        else
+
+					        {
+
+							                if (b < c)
+
+										                        largest = c;
+
+									                else
+
+												                        largest = b;
+
+											        }
+
+				        return (largest);
+
+}
